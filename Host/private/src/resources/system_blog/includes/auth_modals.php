47d6e4a179cb031eee59bcea69243ab4c0319aa8
MRPSouza <contato@matheusrpsouza.com>
2024-11-16 00:57:44 -0300

    feat: remoção do sistema de blog para futura recriação
    - Sistema de blog removido temporariamente.
    - Planejamento para recriação com melhorias.

--- Host/private/src/resources/system_blog/includes/auth_modals.php
+++
@@ -1,9 +0,0 @@
-<!-- Modal de Login -->
-<div class="modal fade" id="loginModal" tabindex="-1">
-    <!-- ... conteúdo do modal de login ... -->
-</div>
-
-<!-- Modal de Cadastro -->
-<div class="modal fade" id="registerModal" tabindex="-1">
-    <!-- ... conteúdo do modal de cadastro ... -->
-</div> 
